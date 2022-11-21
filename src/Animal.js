adfc9d3c2138254ffaf110c1df6f0ef0e018e342
Satyam <starky.satyam@gmail.com>
2022-11-21 22:44:35 +0530

    heart grows big on click

--- src/Animal.js
+++ src/Animal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import cat from './svg/cat.svg'
 import cow from './svg/cow.svg'
 import horse from './svg/horse.svg'
@@ -7,13 +7,18 @@ import gator from './svg/gator.svg'
 import heart from './svg/heart.svg'
 import dog from './svg/dog.svg'
 
-const mapAnimal = {
-    bird, cat, dog, cow, gator, horse
-}
-
 const Animal = ({type}) => {
-    return(<div>
-        <img alt="" src={mapAnimal[type]} />
+    const mapAnimal = {
+        bird, cat, dog, cow, gator, horse
+    }
+    const handleClick = () =>{
+        setClick(click + 1);
+        console.log(click)
+    }
+    const [click, setClick] = useState(0)
+    return(<div onClick={handleClick}>
+        <img alt={type} src={mapAnimal[type]} />
+        <img alt="heart" src={heart} style={{width:`${click*10}px`}}/>
     </div>)
 };
 
